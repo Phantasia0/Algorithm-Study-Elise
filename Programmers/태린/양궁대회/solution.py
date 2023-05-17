c383ab305de90f164aa68ba0a8902c4e7dacde32
[AI7/2반] 손태린 <elice.str@gmail.com>
2023-05-17 18:42:52 +0900

    양궁대회

--- Programmers/태린/양궁대회/solution.py
+++ Programmers/태린/양궁대회/solution.py
@@ -1,43 +1,41 @@
 def solution(n, info):
     answer = [-1]
     lion = [0] * 11
-    max = 0
+    maxD = 0
 
     def comparePoint(lion):
         lion_total = 0
         apeach_total = 0
-        nonlocal max
+        nonlocal maxD, answer
         for p in range(10):
             if lion[p] > info[p]:
                 lion_total += 10-p
             elif info[p]:
                 apeach_total += 10-p
         result = lion_total - apeach_total
-        if max <= result:
-            max = result
-            return True
-        return False
+        if maxD < result:
+            maxD = result
+            answer = lion
+        elif maxD == result and result > 0:
+            if answer[0] == -1:
+                answer = [0] * 11
+            for i in range(10, -1, -1):
+                if lion[i] > 0:
+                    if lion[i] > answer[i]:
+                        answer = lion
+                        return
+                    elif lion[i] == answer[i]:
+                        continue
+                if answer[i] > 0:
+                    return
 
     def getPoint(arrow, lion, stage):
-        nonlocal answer
         if stage == 10:
             lion[stage] = arrow
             arrow = 0
         # 화살이 남았는지 체크
         if arrow == 0:
-            if comparePoint(lion):
-                if answer[0] == -1:
-                    answer = lion
-                else:
-                    for i in range(10, 0, -1):
-                        if lion[i] > 0:
-                            if lion[i] > answer[i]:
-                                answer = lion
-                                return
-                            elif lion[i] == answer[i]:
-                                continue
-                        if answer[i] > 0:
-                            return
+            comparePoint(lion)
             return
         # 화살 쏘고 호출
         if arrow > info[stage]:
